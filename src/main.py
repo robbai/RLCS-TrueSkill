2bc4bb47fe932fbb675f10a0e432b49a3a39097a
Robbie Heath <robbiealternate@gmail.com>
2021-01-26 16:44:42 +0000

    First demonstration

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
+import math
+import itertools
 from json import loads as parse_json
-from time import time
 from typing import Dict, List, Tuple
 
 from tqdm import tqdm
@@ -13,89 +14,147 @@ from requester import get_content
 url: str = "https://api.octane.gg/api/event_list"
 
 
-def event_filter(event: Dict) -> bool:
-    return (event["type"] == "RLCS" or "RLCS" in event["Event"]) and (
-        "North America" in event["Event"]
-        or "Europe" in event["Event"]
-        or "World Championship" in event["Event"]
+# https://github.com/sublee/trueskill/issues/1#issuecomment-149762508
+def win_probability(env: TrueSkill, team1: List[Rating], team2: List[Rating]) -> float:
+    delta_mu: float = sum(r.mu for r in team1) - sum(r.mu for r in team2)
+    sum_sigma: float = sum(r.sigma ** 2 for r in itertools.chain(team1, team2))
+    size: int = len(team1) + len(team2)
+    denom: float = math.sqrt(size * (env.beta * env.beta) + sum_sigma)
+    return env.cdf(delta_mu / denom)
+
+
+def win_probability_best_of(
+    best_of: int, probability: float, score: Tuple[int, int] = (0, 0)
+) -> float:
+    first_to: int = best_of // 2 + 1
+    if score[0] == first_to:
+        return 1
+    elif score[1] == first_to:
+        return 0
+    return probability * win_probability_best_of(
+        best_of, probability, (score[0] + 1, score[1])
+    ) + (1 - probability) * win_probability_best_of(
+        best_of, probability, (score[0], score[1] + 1)
     )
 
 
-if __name__ == "__main__":
-    time_started: float = time()
-    mu_env: float = 2700
-    sigma_env: float = mu_env / 4.5
-    env: TrueSkill = TrueSkill(mu=mu_env, sigma=sigma_env)
+def event_filter(event: Dict) -> bool:
+    # return (event["type"] == "RLCS" or "RLCS" in event["Event"]
+    #         # or "RLRS" in event["Event"]
+    #         ) and (
+    #     "North America" in event["Event"]
+    #     or "Europe" in event["Event"]
+    #     or "World Championship" in event["Event"]
+    # )
+    prize: str = event["prize"]
+    return prize and prize[0] == "$" and float(prize[1:].replace(",", "")) >= 25000
+
+
+def main():
+    env: TrueSkill = TrueSkill(draw_probability=0, backend="mpmath")
     rankings: Dict[str, Rating] = {}
+
+    matches: List[Tuple[str, int]] = []
 
     # Iterate through events.
     main_content: str = get_content(url)
     event_table: List[Dict] = [
         event for event in parse_json(main_content)["data"] if event_filter(event)
     ]
-    for event in event_table:
-        print(event["Event"])
+    for event in tqdm(event_table, desc="Event table"):
 
-        # Iterate through matches.
+        # Add matches.
         matches_url: str = "https://api.octane.gg/api/matches_event/" + event[
             "EventHyphenated"
         ]
         event_content: str = get_content(matches_url)
         match_table: List[Dict] = parse_json(event_content)["data"]
-        for match in tqdm(match_table):
-            invalid_match: bool = False
+        for match in match_table:
             games: int = match["Team1Games"] + match["Team2Games"]
             if games <= 1:
                 continue
+            matches.append((match["match_url"], games))
 
-            # Iterate through games (two teams).
-            for game_number in range(1, games + 1):
-                team_url_format: str = "https://api.octane.gg/api/match_scoreboard_{}/" + match[
-                    "match_url"
-                ] + "/" + str(
-                    game_number
-                )
+    # Iterate through matches.
+    for match_id, games in tqdm(matches[::-1], desc="Match list"):
+        # for match_id, games in matches[::-1]:
+        invalid_match: bool = False
 
-                winner: int = None
-                names: List[List[str]] = [[], []]
-                ratings: List[List[Rating]] = [[], []]
+        # Iterate through games (two teams).
+        for game_number in range(1, games + 1):
+            team_url_format: str = "https://api.octane.gg/api/match_scoreboard_{}/" + match_id + "/" + str(
+                game_number
+            )
 
-                for i, team in enumerate(("one", "two")):
-                    team_url: str = team_url_format.format(team)
+            winner: int = None
+            names: List[List[str]] = [[], []]
+            ratings: List[List[Rating]] = [[], []]
 
-                    try:
-                        team_content: str = get_content(team_url)
-                        team_table: List[Dict] = parse_json(team_content)["data"]
-                        winner = team_table[0]["Winner"]
-                        for name in team_table[:-1]:  # Last "player" is the sum.
-                            names[i].append(name["Player"].title())
-                            if not names[i][-1] in rankings:
-                                rankings[names[i][-1]] = env.create_rating()
-                            ratings[i].append(rankings[names[i][-1]])
-                    except Exception:
-                        invalid_match = True
-                        break
+            for i, team in enumerate(("one", "two")):
+                team_url: str = team_url_format.format(team)
 
-                if invalid_match:
+                try:
+                    team_content: str = get_content(team_url)
+                    team_table: List[Dict] = parse_json(team_content)["data"]
+                    winner = team_table[0]["Winner"]
+                    for name in team_table[:-1]:  # Last "player" is the sum.
+                        names[i].append(name["Player"].title())
+                        if not names[i][-1] in rankings:
+                            rankings[names[i][-1]] = env.create_rating()
+                        ratings[i].append(rankings[names[i][-1]])
+                except Exception:
+                    invalid_match = True
                     break
 
-                ranks = [1, 1]
-                ranks[winner] = 0
-                new_ratings = env.rate(ratings, ranks)
-                for i in range(2):
-                    for j, name in enumerate(names[i]):
-                        rankings[name] = new_ratings[i][j]
+            if invalid_match or not all(len(named) == 3 for named in names):
+                break
 
-                if time() - time_started > 600:
-                    leaderboard: List[Tuple[str, float, float]] = []
-                    for player in sorted(
-                        rankings.items(),
-                        key=lambda player: env.expose(player[1]),
-                        reverse=True,
-                    ):
-                        # if player[1].sigma > sigma_env / 2: continue
-                        leaderboard.append(
-                            (player[0], str(player[1].mu), str(player[1].sigma))
-                        )
-                    print(tabulate(leaderboard, headers=["Name", "Mu", "Sigma"]))
-                    exit()
+            ranks = [1, 1]
+            ranks[winner] = 0
+            new_ratings = env.rate(ratings, ranks)
+            for i in range(2):
+                for j, name in enumerate(names[i]):
+                    rankings[name] = new_ratings[i][j]
+    print()
+
+    # Print the leaderboard.
+    leaderboard: List[Tuple[str, float, float]] = []
+    for player in sorted(
+        rankings.items(), key=lambda player: env.expose(player[1]), reverse=True,
+    ):
+        if player[1].sigma > 3:
+            continue
+        leaderboard.append((player[0], str(player[1].mu), str(player[1].sigma)))
+    print(tabulate(leaderboard, headers=["Name", "Mu", "Sigma"]))
+    print()
+
+    # Input loop
+    while True:
+        ratings: List[List[Rating]] = [[], []]
+        for i in range(6):
+            while True:
+                index: int = i % 3
+                team: int = i // 3
+                name = input(
+                    "Player " + str(index + 1) + " on team " + str(team + 1) + ": "
+                ).title()
+                if name in rankings:
+                    ratings[team].append(rankings[name])
+                    break
+                else:
+                    print("Couldn't find player")
+        probability: float = win_probability(env, *ratings)
+        print("Win probabilities:")
+        for best_of in range(1, 8, 2):
+            print(
+                "Team 1 in BO"
+                + str(best_of)
+                + ": "
+                + str(round(win_probability_best_of(best_of, probability) * 100, 2))
+                + "%"
+            )
+        print()
+
+
+if __name__ == "__main__":
+    main()
--- src/requester.py
+++ src/requester.py
@@ -1,18 +1,20 @@
 from re import sub
 from pickle import dump, load
+from typing import Tuple, Union
 from os.path import isfile
 
 from requests import get as request
 
 
-def get_content(url: str) -> str:
+def get_content(url: str, return_cached: bool = False) -> Union[str, Tuple[str, bool]]:
     page = None
     file_name: str = "cache/" + sub(
         r"[^a-zA-Z0-9_ ]+", "", url.replace("/", "_")
     ) + ".obj"
 
     # Cache.
-    if isfile(file_name):
+    cached: bool = isfile(file_name)
+    if cached:
         # print("Retrieving '" + url + "' from cache")
         file = open(file_name, "rb")
         page = load(file)
@@ -25,4 +27,6 @@ def get_content(url: str) -> str:
         dump(page, file)
         file.close()
 
+    if return_cached:
+        return page.content.decode("utf-8"), cached
     return page.content.decode("utf-8")
