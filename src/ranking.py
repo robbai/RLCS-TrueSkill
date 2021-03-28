781a90867864c5b7ee9bf31ca24a349562089d09
Robbie Heath <robbiealternate@gmail.com>
2021-03-28 14:48:19 +0100

    Cache older things

--- src/ranking.py
+++ src/ranking.py
@@ -1,15 +1,21 @@
 from json import loads as parse_json
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Optional
+from datetime import datetime as dtime
+from datetime import timedelta
 
 from tqdm import tqdm
 from trueskill import Rating, TrueSkill
 
-from requester import get_content, remove_cache
+from requester import cache, get_content, remove_cache
 
 # Archive/Unfinished -> Event -> Match -> Game
 
 unfinished_url: str = "https://api.octane.gg/api/event_list/upcoming"
 archive_url: str = "https://api.octane.gg/api/event_list"
+
+# Cache events and invalid matches that are older than this.
+# All valid matches are cached.
+CACHE_TIME: dtime = dtime.now() - timedelta(days=90)
 
 
 def event_filter(event: Dict) -> bool:
@@ -34,6 +40,10 @@ def fix_player_name(name: str) -> str:
     return name
 
 
+def parse_date(data: Dict) -> dtime:
+    return dtime.strptime(data["Date"].split("T", 1)[0], "%Y-%m-%d")
+
+
 def get_matches() -> List[Tuple[str, int, bool]]:
     matches: List[Tuple[str, int, bool]] = []
 
@@ -41,7 +51,7 @@ def get_matches() -> List[Tuple[str, int, bool]]:
         unfinished: bool = (url == unfinished_url)
 
         # Iterate through events.
-        main_content: str = get_content(url, can_cache=False)
+        main_content: str = get_content(url)
         event_table: List[Dict] = [
             event for event in parse_json(main_content)["data"] if event_filter(event)
         ]
@@ -53,25 +63,24 @@ def get_matches() -> List[Tuple[str, int, bool]]:
             matches_url: str = "https://api.octane.gg/api/matches_event/" + event[
                 "EventHyphenated"
             ]
-            event_content: str = get_content(matches_url, can_cache=False)
+            event_content: str = get_content(matches_url)
             event_matches: List[Tuple[str, int, bool]] = []
             try:
                 match_table: List[Dict] = parse_json(event_content)["data"]
+                if parse_date(match_table[0]) < CACHE_TIME:
+                    cache(matches_url, event_content)
                 for match in match_table:
                     games: int = match["Team1Games"] + match["Team2Games"]
                     if games <= 1:
                         continue
                     event_matches.append((match["match_url"], games, unfinished))
             except Exception:
-                remove_cache(matches_url)
                 continue
             if event_matches:
                 matches += event_matches
                 event_results.append(
                     event["Event"] + " (" + str(len(event_matches)) + ")"
                 )
-            else:
-                remove_cache(matches_url)
         print("\n".join(event_results))
 
     return matches
@@ -98,9 +107,12 @@ def setup_ranking(env: TrueSkill, rankings: Dict[str, Rating]):
             for i, team in enumerate(("one", "two")):
                 team_url: str = team_url_format.format(team)
 
+                team_date: Optional[dtime] = None
                 try:
-                    team_content: str = get_content(team_url, can_cache=not unfinished)
+                    team_content: str = get_content(team_url)
                     team_table: List[Dict] = parse_json(team_content)["data"]
+                    cache(team_url, team_content)
+                    team_date = parse_date(team_table[0])
                     winner = i if team_table[0]["Winner"] else not i
                     for name in team_table[:-1]:  # Last "player" is the sum.
                         title_name: str = name["Player"].title().strip()
@@ -111,7 +123,8 @@ def setup_ranking(env: TrueSkill, rankings: Dict[str, Rating]):
                         ratings[i].append(rankings[title_name])
                 except Exception:
                     invalid_match = True
-                    remove_cache(team_url)
+                    if not team_date or team_date >= CACHE_TIME:
+                        remove_cache(team_url)
                     break
 
             if invalid_match or any(len(named) != 3 for named in names):
--- src/requester.py
+++ src/requester.py
@@ -9,23 +9,28 @@ def get_cache_name(url: str) -> str:
     return "cache/" + sub(r"[^a-zA-Z0-9_ ]+", "", url.replace("/", "_")) + ".json"
 
 
-def get_content(url: str, can_cache: bool = True) -> str:
+def get_content(url: str) -> str:
     file_name: str = get_cache_name(url)
 
     # Cache.
-    if can_cache and isfile(file_name):
+    if isfile(file_name):
         file = open(file_name, "r")
         content = file.read()
         file.close()
     else:
         # Request.
         content = request(url).content.decode("utf-8")
-        if can_cache:
-            file = open(file_name, "w")
-            file.write(content)
-            file.close()
 
     return content
+
+
+def cache(url: str, content, overwrite: bool = False):
+    file_name: str = get_cache_name(url)
+    if not overwrite and isfile(file_name):
+        return
+    file = open(file_name, "w")
+    file.write(content)
+    file.close()
 
 
 def remove_cache(url: str):
